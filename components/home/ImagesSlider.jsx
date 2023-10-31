6794fedd021b3e09f575b71342fd473bcc6e85f0
wizacne <tungh3210@gmail.com>
2023-10-31 16:57:52 +0700

    lay hinh sliderbox

--- components/home/ImagesSlider.jsx
+++ components/home/ImagesSlider.jsx
@@ -1,16 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { COLORS, SIZES } from "../../constants";
-import { SliderBox } from "react-native-image-slider-box";
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { COLORS, SIZES } from '../../constants';
+import { SliderBox } from 'react-native-image-slider-box';
 
 const ImagesSlider = () => {
   const slides = [
-    "https://wallpaperset.com/w/full/f/d/6/43388.jpg",
-    "https://wallpaperset.com/w/full/3/3/6/186736.jpg",
-    "https://wallpaperset.com/w/full/a/2/9/186945.jpg",
-    "https://wallpaperset.com/w/full/2/b/1/186688.jpg",
-    "https://wallpaperset.com/w/full/a/4/b/76571.jpg",
-    "https://wallpaperset.com/w/full/2/5/b/408920.jpg",
+    'https://wallpaperset.com/w/full/f/d/6/43388.jpg',
+    'https://wallpaperset.com/w/full/3/3/6/186736.jpg',
+    'https://wallpaperset.com/w/full/a/2/9/186945.jpg',
+    'https://wallpaperset.com/w/full/2/b/1/186688.jpg',
+    'https://wallpaperset.com/w/full/a/4/b/76571.jpg',
+    'https://wallpaperset.com/w/full/2/5/b/408920.jpg',
   ];
   return (
     <View style={styles.imageSlider}>
@@ -26,7 +26,7 @@ const ImagesSlider = () => {
           margin: 0,
         }}
         inactiveDotColor={COLORS.secondary}
-        ImageComponentStyle={{ borderRadius: 15, width: "95%", marginTop: 15 }}
+        ImageComponentStyle={{ borderRadius: 15, width: '95%', marginTop: 15 }}
         autoplay
         circleLoop
       />
@@ -39,6 +39,6 @@ export default ImagesSlider;
 const styles = StyleSheet.create({
   imageSlider: {
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
   },
 });
