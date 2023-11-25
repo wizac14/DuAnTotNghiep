326e1c7fb217b01020658f652199696f2f1785e7
wizacne <tungh3210@gmail.com>
2023-11-26 00:12:16 +0700

    done person screen

--- screens/Home/Profile.jsx
+++ screens/Home/Profile.jsx
@@ -1,4 +1,13 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput, Button, Pressable } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  Button,
+  Pressable,
+} from 'react-native';
 import React, { useContext, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { SIZES } from '../../constants/index';
@@ -16,7 +25,7 @@ const Profile = () => {
   const navigation = useNavigation();
   const { inforuser, setinforuser } = useContext(AppContext);
   const handleSubmit = async () => {
-    const response = await AxiosIntance().put('/user/update/'+ inforuser._id,{
+    const response = await AxiosIntance().post('/user/update', {
       name: inforuser.name,
       email: inforuser.email,
       address: inforuser.address,
@@ -48,7 +57,7 @@ const Profile = () => {
 
   const phoneValidation = yup
     .string()
-    .matches(/^(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b/, 'Số điện thoại không hợp lệ');
+    .matches(/^\+84[3|5|7|8|9][0-9]{8}\b/, 'Số điện thoại không hợp lệ (bắt đầu với +84)');
 
   const validationSchema = yup.object().shape({
     email: emailValidation,
@@ -66,14 +75,14 @@ const Profile = () => {
 
   return (
     <View style={styles.container}>
-      <View style={{ flexDirection: 'row', top: "10%"}}>
+      <View style={{ flexDirection: 'row', top: '10%' }}>
         <View>
           <TouchableOpacity
             onPress={() => {
               navigation.goBack();
             }}
           >
-            <Ionicons name="arrow-back-outline" size={30}/>
+            <Ionicons name="arrow-back-outline" size={30} />
           </TouchableOpacity>
         </View>
         <View>
@@ -86,58 +95,62 @@ const Profile = () => {
         validationSchema={validationSchema}
       >
         {({ handleChange, handleBlur, handleSubmit, values, touched, errors }) => (
-          <View > 
+          <View>
             <View style={styles.viewItem}>
-              <TextInput style={styles.textHint} placeholder="Email" value={values.email} editable={false} />
+              <TextInput
+                style={styles.textHint}
+                placeholder="Email"
+                value={values.email}
+                editable={false}
+              />
               {touched.email && errors.email && <Text style={styles.error}>{errors.email}</Text>}
             </View>
-            <View style={styles.viewItem} >
-            <TextInput
-              style={styles.textHint}
-              placeholder="Name"
-              onChangeText={(text) => {
-                handleChange('name')(text);
-                setinforuser({ ...inforuser, name: text });
-              }}
-              // onBlur={handleBlur('name')}
-              value={inforuser.name}
-            />
-            {touched.name && errors.name && <Text style={styles.error}>{errors.name}</Text>}
-
-            </View> 
+            <View style={styles.viewItem}>
+              <TextInput
+                style={styles.textHint}
+                placeholder="Name"
+                onChangeText={(text) => {
+                  handleChange('name')(text);
+                  setinforuser({ ...inforuser, name: text });
+                }}
+                // onBlur={handleBlur('name')}
+                value={inforuser.name}
+              />
+              {touched.name && errors.name && <Text style={styles.error}>{errors.name}</Text>}
+            </View>
 
             <View style={styles.viewItem}>
-            <TextInput
-              style={styles.textHint}
-              placeholder="Sđt"
-              onChangeText={(text) => {
-                handleChange('phoneNumber')(text);
-                setinforuser({ ...inforuser, phoneNumber: text });
-              }}
-              // onBlur={handleBlur('phoneNumber')}
-              value={inforuser.phoneNumber}
-            />
-            {touched.phoneNumber && errors.phoneNumber && (
-              <Text style={styles.error}>{errors.phoneNumber}</Text>
-            )}
+              <TextInput
+                style={styles.textHint}
+                placeholder="Sđt"
+                onChangeText={(text) => {
+                  handleChange('phoneNumber')(text);
+                  setinforuser({ ...inforuser, phoneNumber: text });
+                }}
+                // onBlur={handleBlur('phoneNumber')}
+                value={inforuser.phoneNumber}
+              />
+              {touched.phoneNumber && errors.phoneNumber && (
+                <Text style={styles.error}>{errors.phoneNumber}</Text>
+              )}
             </View>
             <View style={styles.viewItem}>
-            <TextInput
-              style={styles.textHint}
-              placeholder="Địa chỉ"
-              onChangeText={(text) => {
-                handleChange('address')(text);
-                setinforuser({ ...inforuser, address: text });
-              }}
-              // onBlur={handleBlur('address')}
-              value={inforuser.address}
-            />
-            {touched.address && errors.address && (
-              <Text style={styles.error}>{errors.address}</Text>
-            )}
+              <TextInput
+                style={styles.textHint}
+                placeholder="Địa chỉ"
+                onChangeText={(text) => {
+                  handleChange('address')(text);
+                  setinforuser({ ...inforuser, address: text });
+                }}
+                // onBlur={handleBlur('address')}
+                value={inforuser.address}
+              />
+              {touched.address && errors.address && (
+                <Text style={styles.error}>{errors.address}</Text>
+              )}
             </View>
             <Pressable style={styles.btn} onPress={handleSubmit}>
-            <Text style={styles.btnUpdate}>CẬP NHẬT THÔNG TIN</Text>
+              <Text style={styles.btnUpdate}>CẬP NHẬT THÔNG TIN</Text>
             </Pressable>
           </View>
         )}
@@ -158,7 +171,7 @@ const styles = StyleSheet.create({
   },
   error: {
     color: 'red',
-    marginTop:5
+    marginTop: 5,
   },
   title: {
     fontSize: 22,
@@ -170,7 +183,7 @@ const styles = StyleSheet.create({
   viewItem: {
     marginTop: 25,
     height: 55,
-    width:'100%',
+    width: '100%',
     backgroundColor: '#F5F7F8',
     borderRadius: 10,
   },
@@ -182,7 +195,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   btn: {
-    marginTop:50,
+    marginTop: 50,
     backgroundColor: 'black',
     height: 50,
     borderRadius: 10,
