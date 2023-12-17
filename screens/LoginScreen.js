1d0dc05661c5eb4bb3626fdc1adb0890f0c67125
DZViet <117284050+DZViet@users.noreply.github.com>
2023-12-17 23:19:08 +0700

    Validation login, Register

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -10,6 +10,8 @@ import { AppContext } from '../components/ultil/AppContext';
 import { useContext } from 'react';
 import { Ionicons, Fontisto } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
 export default function LoginScreen() {
   const [email, setEmail] = useState('');
@@ -19,7 +21,24 @@ export default function LoginScreen() {
   const [isSecureEntry, setIsSecureEntry] = useState(true);
   const { setisLogin, setinforuser } = useContext(AppContext);
 
-  const LoginUser = async () => {
+  const emailValidation = yup
+    .string()
+    .email('Email không hợp lệ')
+    .matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/, 'Email không hợp lệ');
+
+  const passwordValidation = yup
+    .string()
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, 'Mật khẩu chưa phù hợp');
+  const initialValues = {
+    email: email,
+    password: password,
+  };
+  const validationSchema = yup.object().shape({
+    email: emailValidation,
+    password: passwordValidation,
+  });
+
+  const handleSubmit = async () => {
     try {
       const response = await AxiosIntance().post('/user/login', {
         email: email,
@@ -53,72 +72,94 @@ export default function LoginScreen() {
             Đăng nhập
           </Animated.Text>
         </View>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validationSchema={validationSchema}
+        >
+          {({ handleChange, handleBlur, handleSubmit, touched, errors }) => (
+            <View className="flex items-center mx-5 space-y-4">
+              <Animated.View
+                entering={FadeInDown.duration(1000).springify()}
+                className="bg-black/5 p-5 rounded-2xl w-full"
+              >
+                <TextInput
+                  placeholderTextColor={'gray'}
+                  value={email}
+                  placeholder="Email"
+                  onChangeText={(text) => {
+                    handleChange('email')(text);
+                    setEmail(text);
+                  }}
+                />
+                {touched.email && errors.email && (
+                  <Text style={{ marginTop: 5, color: 'red' }}>{errors.email}</Text>
+                )}
+              </Animated.View>
+              <Animated.View
+                entering={FadeInDown.delay(200).duration(1000).springify()}
+                className="bg-black/5 p-4 rounded-2xl w-full  justify-between flex-row"
+              >
+                <TextInput
+                  value={password}
+                  placeholder="Mật khẩu"
+                  placeholderTextColor={'gray'}
+                  onChangeText={(text) => {
+                    handleChange('password')(text);
+                    setPassword(text);
+                  }}
+                  secureTextEntry={isSecureEntry}
+                />
+                {touched.password && errors.password && (
+                  <Text style={{ marginTop: 5, color: 'red' }}>{errors.password}</Text>
+                )}
+                <Ionicons
+                  style={{ padding: 5 }}
+                  name={isSecureEntry ? 'eye' : 'eye-off'}
+                  size={20}
+                  color="grey"
+                  onPress={() => setIsSecureEntry(!isSecureEntry)}
+                />
+              </Animated.View>
 
-        <View className="flex items-center mx-5 space-y-4">
-          <Animated.View
-            entering={FadeInDown.duration(1000).springify()}
-            className="bg-black/5 p-5 rounded-2xl w-full"
-          >
-            <TextInput
-              placeholderTextColor={'gray'}
-              value={email}
-              onChangeText={(text) => setEmail(text)}
-              placeholder="Email"
-            />
-          </Animated.View>
-          <Animated.View
-            entering={FadeInDown.delay(200).duration(1000).springify()}
-            className="bg-black/5 p-4 rounded-2xl w-full  justify-between flex-row"
-          >
-            <TextInput
-              value={password}
-              onChangeText={(text) => setPassword(text)}
-              secureTextEntry={isSecureEntry}
-              placeholder="Mật khẩu"
-              placeholderTextColor={'gray'}
-            />
-            <Ionicons
-              style={{ padding: 5 }}
-              name={isSecureEntry ? 'eye' : 'eye-off'}
-              size={20}
-              color="grey"
-              onPress={() => setIsSecureEntry(!isSecureEntry)}
-            />
-          </Animated.View>
+              <Animated.View
+                entering={FadeInDown.delay(600).duration(1000).springify()}
+                className="flex-row items-end justify-end"
+                style={{
+                  justifyContent: 'flex-end',
+                  alignItems: 'flex-end',
+                  alignSelf: 'flex-end',
+                }}
+              >
+                <TouchableOpacity onPress={() => navigation.push('Forgot Password')}>
+                  <Text className="text-red-600">Quên mật khẩu?</Text>
+                </TouchableOpacity>
+              </Animated.View>
 
-          <Animated.View
-            entering={FadeInDown.delay(600).duration(1000).springify()}
-            className="flex-row items-end justify-end"
-            style={{
-              justifyContent: 'flex-end',
-              alignItems: 'flex-end',
-              alignSelf: 'flex-end',
-            }}
-          >
-            <TouchableOpacity onPress={() => navigation.push('Forgot Password')}>
-              <Text className="text-red-600">Quên mật khẩu?</Text>
-            </TouchableOpacity>
-          </Animated.View>
+              <Animated.View
+                className="w-full"
+                entering={FadeInDown.delay(400).duration(1000).springify()}
+              >
+                <TouchableOpacity
+                  onPress={handleSubmit}
+                  className="w-full bg-red-700 p-3 rounded-2xl "
+                >
+                  <Text className="text-xl font-bold text-white text-center">Đăng nhập</Text>
+                </TouchableOpacity>
+              </Animated.View>
 
-          <Animated.View
-            className="w-full"
-            entering={FadeInDown.delay(400).duration(1000).springify()}
-          >
-            <TouchableOpacity onPress={LoginUser} className="w-full bg-red-700 p-3 rounded-2xl ">
-              <Text className="text-xl font-bold text-white text-center">Đăng nhập</Text>
-            </TouchableOpacity>
-          </Animated.View>
-
-          <Animated.View
-            entering={FadeInDown.delay(600).duration(1000).springify()}
-            className="flex-row justify-center"
-          >
-            <Text>Chưa có tài khoản? </Text>
-            <TouchableOpacity onPress={() => navigation.push('Register')}>
-              <Text className="text-red-600">Đăng ký ngay!</Text>
-            </TouchableOpacity>
-          </Animated.View>
-        </View>
+              <Animated.View
+                entering={FadeInDown.delay(600).duration(1000).springify()}
+                className="flex-row justify-center"
+              >
+                <Text>Chưa có tài khoản? </Text>
+                <TouchableOpacity onPress={() => navigation.push('Register')}>
+                  <Text className="text-red-600">Đăng ký ngay!</Text>
+                </TouchableOpacity>
+              </Animated.View>
+            </View>
+          )}
+        </Formik>
       </View>
     </View>
   );
